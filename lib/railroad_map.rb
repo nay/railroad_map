3134d61c5e653d9e40923ece5e3463708063abda
Yasuko Ohba <nay@meadowy.org>
2011-01-26 17:55:17 +0900

    refactored for customizations

--- lib/railroad_map.rb
+++ lib/railroad_map.rb
@@ -1,14 +1,162 @@
 # RailroadMap
-# 先に application_controller.rbを読み込みたいのでこう書いている
+# need to load application_controller.rb first
 ApplicationController.class_eval do
-
-  def self.analyze_action_methods
-    result = {} # modules per action
+  def self.action_methods_analysis
+    return @action_methods_analysis if @action_methods_analysis
+    @action_methods_analysis = {} # modules per action
     action_methods.each do |a|
       modules = ancestors.find_all{|m| !ApplicationController.ancestors.include?(m) && m.instance_methods(false).include?(a)}
-      result[a.to_s] = modules
+      @action_methods_analysis[a.to_s] = modules
     end
-    return result
+    @action_methods_analysis
   end
 
+  def self.named_before_filters
+    @named_before_filters ||= filter_chain.find_all{|f| f.kind_of?(ActionController::Filters::BeforeFilter) && f.method.kind_of?(Symbol)}
+  end
+
+  def self.named_partial_before_filters
+    @named_partial_before_filters ||= named_before_filters.find_all{|f| f.options[:only] || f.options[:except]}
+  end
+
+  def self.named_general_before_filters
+    @named_general_before_filters ||= named_before_filters - named_partial_before_filters
+  end
+end
+
+# output schem
+module RailroadMap
+
+  class ControllerDetail
+    attr_accessor :controller
+
+    private
+    def before_filters
+      raise "no controller" unless controller
+      @before_filters ||= controller.filter_chain.find_all{|f| f.kind_of?(ActionController::Filters::BeforeFilter) && f.method.kind_of?(Symbol)}
+    end
+
+  end
+
+  class ControllerGeneralBeforeFilters < ControllerDetail
+    def write_headline_to(out)
+      out.write("<h2>General Before Filters</h2>\n")
+    end
+    def write_contents_to(out)
+      out.write("<ul>\n")
+      controller.named_general_before_filters.each do |f|
+        out.write("<li>#{f.method}</li>\n")
+      end
+      out.write("</ul>\n")
+    end
+  end
+
+  class ControllerActions < ControllerDetail
+
+    def controller=(c)
+      super
+      @action_attributes = RailroadMap.action_attributes.each {|attr| attr.controller = c}
+    end
+
+    def write_headline_to(out)
+      out.write("<h2>Actions</h2>\n")
+    end
+
+    def write_contents_to(out)
+      out.write("<table border='1'>\n")
+      out.write("<tr>\n")
+      out.write("<th>No.</th>")
+      action_attributes.each do |attr|
+        attr.write_headers_to(out)
+      end
+      out.write("\n")
+
+      out.write("</tr>\n")
+      sorted_actions.each_with_index do |a, pos|
+        action_attributes.each {|attr| attr.action = a}
+        out.write("<tr>\n")
+        out.write("<td class='number'>#{pos+1}</td>")
+        action_attributes.each do |attr|
+          attr.write_contents_to(out)
+        end
+
+        out.write("\n")
+        out.write("</tr>\n")
+      end
+      out.write("</table>\n")
+    end
+
+    private
+    def action_attributes
+      @action_attributes
+    end
+
+    def sorted_actions
+      controller.action_methods.sort do |a, b|
+        if controller.action_methods_analysis[a.to_s].to_s == controller.action_methods_analysis[b.to_s].to_s
+          a <=> b
+        elsif controller.action_methods_analysis[a.to_s].to_s == controller.to_s
+          -1
+        elsif controller.action_methods_analysis[b.to_s].to_s == controller.to_s
+          1
+        else
+          controller.action_methods_analysis[a.to_s].to_s <=> controller.action_methods_analysis[b.to_s].to_s
+        end
+      end
+    end
+  end
+
+
+  class BaseActionAttribute < ControllerDetail
+    attr_accessor :action
+
+
+  end
+
+  class ActionName < BaseActionAttribute
+    def write_headers_to(out)
+      out.write("<th>Action Name</th>")
+    end
+    def write_contents_to(out)
+      out.write("<td>#{action}</td>")
+    end
+  end
+
+  class ActionImplementedIn < BaseActionAttribute
+    def write_headers_to(out)
+      out.write("<th>Implemented in</th>")
+    end
+    def write_contents_to(out)
+      out.write("<td>#{(controller.action_methods_analysis[action.to_s]).map{|m| m.to_s}.join('<br />')}</td>")
+    end
+  end
+
+  class ActionPartialFilters < BaseActionAttribute
+    def write_headers_to(out)
+      controller.named_partial_before_filters.each do |f|
+        separated_method_name = f.method.to_s.gsub(/_/, ' _')
+        out.write("<th style='width:100px; font-size: 80%;'>#{separated_method_name}</th>")
+      end
+    end
+
+    def write_contents_to(out)
+      controller.named_partial_before_filters.each do |f|
+        applied = (f.options[:only] && f.options[:only].include?(action.to_s)) || (f.options[:except] && !f.options[:except].include?(action.to_s))
+        out.write("<td class='#{applied ? 'applied' : 'notApplied'}'  style='width:100px;'>#{applied ? 'applied' : '-'}</td>")
+      end
+   end
+
+  end
+
+  CONTROLLER_DETAILS = [ControllerGeneralBeforeFilters, ControllerActions]
+  ACTION_INFO = [ActionName, ActionImplementedIn, ActionPartialFilters]
+
+  # You can override these
+  def self.controller_details
+    CONTROLLER_DETAILS.map{|clazz| clazz.new}
+  end
+
+  def self.action_attributes
+    ACTION_INFO.map{|clazz| clazz.new}
+  end
 end
--- lib/tasks/railroad_map.rake
+++ lib/tasks/railroad_map.rake
@@ -27,9 +27,14 @@ namespace :railroad_map do
 
     # generate controller docs
     FileUtils.mkpath(File.join(RAILS_ROOT, 'doc', 'railroad_map', 'controllers'))
+
+    action_attributes = RailroadMap.action_attributes
+
     controllers.each do |c|
 
-      action_analysis = c.analyze_action_methods
+      action_attributes.each {|attr| attr.controller = c}
+
+      action_analysis = c.action_methods_analysis
 
       path = File.join(RAILS_ROOT, 'doc', 'railroad_map', 'controllers', *c.to_s.underscore.split('/'))
       FileUtils.mkpath(path)
@@ -38,52 +43,45 @@ namespace :railroad_map do
         file.write("<head><title>#{RAILS_ROOT.split('/').last.humanize} Railroad Map Document - #{c.to_s}</title><style>th {text-align: left; padding:2px;} .applied {background-color: #ffa} .number {text-align: right; padding: 0 2px;}</style></head>\n")
         file.write("<body>\n")
         file.write("<h1>#{c.to_s}</h1>\n")
-        # get filter_names
-        before_filters = c.filter_chain.find_all{|f| f.kind_of?(ActionController::Filters::BeforeFilter) && f.method.kind_of?(Symbol)}
-        partial_filters = before_filters.find_all{|f| f.options[:only] || f.options[:except]}
-        general_filters = before_filters - partial_filters
-        file.write("<h2>General Before Filters</h2>\n")
-        file.write("<ul>\n")
-        general_filters.each do |f|
-          file.write("<li>#{f.method}</li>\n")
-        end
-        file.write("</ul>\n")
-        file.write("<h2>Actions</h2>\n")
-        file.write("<table border='1'>\n")
-        file.write("<tr>\n")
-        file.write("<th>No.</th>")
-        file.write("<th>Action Name</th>")
-        file.write("<th>Implemented in</th>")
-        partial_filters.each do |f|
-          separated_method_name = f.method.to_s.gsub(/_/, ' _')
-          file.write("<th style='width:100px; font-size: 80%;'>#{separated_method_name}</th>")
-        end
-        file.write("\n")
 
-        file.write("</tr>\n")
-        c.action_methods.sort do |a, b|
-          if action_analysis[a.to_s].to_s == action_analysis[b.to_s].to_s
-            a <=> b
-          elsif action_analysis[a.to_s].to_s == c.to_s
-            -1
-          elsif action_analysis[b.to_s].to_s == c.to_s
-            1
-          else
-            action_analysis[a.to_s].to_s <=> action_analysis[b.to_s].to_s
-          end
-        end.each_with_index do |a, pos|
-          file.write("<tr>\n")
-          file.write("<td class='number'>#{pos+1}</td>")
-          file.write("<td>#{a}</td>")
-          file.write("<td>#{(action_analysis[a.to_s]).map{|m| m.to_s}.join('<br />')}</td>")
-          partial_filters.each do |f|
-            applied = (f.options[:only] && f.options[:only].include?(a.to_s)) || (f.options[:except] && !f.options[:except].include?(a.to_s))
-            file.write("<td class='#{applied ? 'applied' : 'notApplied'}'  style='width:100px;'>#{applied ? 'applied' : '-'}</td>")
-          end
-          file.write("\n")
-          file.write("</tr>\n")
+        RailroadMap.controller_details.each do |detail|
+          detail.controller = c
+          detail.write_headline_to(file)
+          detail.write_contents_to(file)
         end
-        file.write("</table>\n")
+
+#        file.write("<h2>Actions</h2>\n")
+#        file.write("<table border='1'>\n")
+#        file.write("<tr>\n")
+#        file.write("<th>No.</th>")
+#        action_attributes.each do |attr|
+#          attr.write_headers_to(file)
+#        end
+#        file.write("\n")
+#
+#        file.write("</tr>\n")
+#        c.action_methods.sort do |a, b|
+#          if action_analysis[a.to_s].to_s == action_analysis[b.to_s].to_s
+#            a <=> b
+#          elsif action_analysis[a.to_s].to_s == c.to_s
+#            -1
+#          elsif action_analysis[b.to_s].to_s == c.to_s
+#            1
+#          else
+#            action_analysis[a.to_s].to_s <=> action_analysis[b.to_s].to_s
+#          end
+#        end.each_with_index do |a, pos|
+#          action_attributes.each {|attr| attr.action = a}
+#          file.write("<tr>\n")
+#          file.write("<td class='number'>#{pos+1}</td>")
+#          action_attributes.each do |attr|
+#            attr.write_contents_to(file)
+#          end
+#
+#          file.write("\n")
+#          file.write("</tr>\n")
+#        end
+#        file.write("</table>\n")
         file.write("</body>\n")
         file.write("</html>\n")
       end
